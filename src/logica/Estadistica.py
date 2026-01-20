9b6ed068971ea4810d14eb0361c14dabe01d030a
RobelUC <73531878@continental.edu.pe>
2026-01-19 19:01:32 -0500

    Test un solo elemento (Verde)

--- src/logica/Estadistica.py
+++ src/logica/Estadistica.py
@@ -2,4 +2,8 @@ class Estadistica:
     def calcular_desviacion_estandar(self, numeros):
         if not numeros:
             return 0.0
+        
+        if len(numeros) == 1:
+            return 0.0
+            
         return None
