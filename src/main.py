4162f3b0384b05088be4c55df8f03b9f8ef33544
RobelUC <73531878@continental.edu.pe>
2026-01-19 19:07:12 -0500

    Prueba final ingresando datos (Verde)

---
+++ src/main.py
@@ -0,0 +1,40 @@
+import sys
+import os
+
+# Truco para que Python encuentre la carpeta src si ejecutamos desde aquí
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from src.logica.Estadistica import Estadistica
+
+def main():
+    print("DESVIACIÓN ESTÁNDAR")
+    print("Ingresa los números uno por uno.")
+    print("Escribe 'x'  para calcular la desviación.\n")
+
+    numeros = []
+    
+    while True:
+        entrada = input("Ingresa un número: ")
+        
+        if entrada.lower() in ['x', 'calcular']:
+            break
+        
+        try:
+            numero = float(entrada)
+            numeros.append(numero)
+        except ValueError:
+            print("Por favor, ingresa un número válido.")
+
+    if numeros:
+        estadistica = Estadistica()
+        resultado = estadistica.calcular_desviacion_estandar(numeros)
+        
+        print("\n----------------RESULTADOS----------------")
+        print(f"Números ingresados: {numeros}")
+        print(f"Cantidad de datos (N): {len(numeros)}")
+        print(f"Desviación Estándar: {resultado:.4f}")
+    else:
+        print("No ingresaste ningún número.")
+
+if __name__ == "__main__":
+    main()
