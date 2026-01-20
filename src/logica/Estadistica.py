2900d2f00c2251cffae8e4ecc689525af6eb8bee
RobelUC <73531878@continental.edu.pe>
2026-01-19 19:02:35 -0500

    Con más de un dato (Verde)

--- src/logica/Estadistica.py
+++ src/logica/Estadistica.py
@@ -1,9 +1,16 @@
 class Estadistica:
     def calcular_desviacion_estandar(self, numeros):
+    
         if not numeros:
             return 0.0
         
-        if len(numeros) == 1:
-            return 0.0
+        n = len(numeros)
+    
+        promedio = sum(numeros) / n
+        
+        suma_cuadrados = 0
+        for x in numeros:
+            suma_cuadrados += (x - promedio) ** 2
             
-        return None
+        varianza = suma_cuadrados / n
+        return varianza ** 0.5
